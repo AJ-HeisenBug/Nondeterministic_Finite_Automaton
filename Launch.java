b65dd560d7bf13b845b8bebb5304957730f0b7d6
Devops_Mika <70478810+Devops-Mika@users.noreply.github.com>
2020-09-08 02:29:57 +0200

    Add files via upload

---
+++ Launch.java
@@ -0,0 +1,63 @@
+/**
+ * Class launch provides the required data for NFA.java
+ * Also contains the main method
+ */
+
+import java.io.*;
+import java.util.Set;
+
+public class Launch<S, A> {
+    /**
+     * Gets the transitions required to build a NFA
+     * @param nfa the NFA to build
+     */
+    public static void getTransitions(NFA<Integer, String> nfa){
+        try {
+            BufferedReader myReader = new BufferedReader(new FileReader("trans"));
+            String tmp;
+            while ((tmp = myReader.readLine()) != null) {
+                String[] data = tmp.split(" ");
+                nfa.addTransition(Integer.valueOf(data[0]), data[1], Integer.valueOf(data[2]));
+            }
+            myReader.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * reads the words from 2020_H09_input
+     * @param eingabe the List that stores the data
+     */
+    public static void read_words(char[][] eingabe){
+        try {
+            BufferedReader myReader = new BufferedReader(new FileReader("input"));
+            String tmp; 
+            int index = 0;
+            while ((tmp = myReader.readLine()) != null) {
+                eingabe[index] = tmp.toCharArray();
+                index++;
+            }
+            myReader.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        NFA<Integer, String> nfa = new NFA<>();
+        getTransitions(nfa);
+        char[][] eingabe = new char[4][];
+        read_words(eingabe);   
+        for (char[] e : eingabe){
+            // long z = System.currentTimeMillis();
+            Set<Integer> reachable = nfa.simulate(7, e);
+            System.out.println(reachable);
+            // System.out.println(System.currentTimeMillis()-z);
+        }
+    }
+}
---
+++ NFA.java
@@ -0,0 +1,72 @@
+/**
+ * #Nondeterministic_Finite_Automaton
+ * Simulates a NFA.
+ * 
+ * This is an implementation of a Non deterministic finite automaton. 
+ * The program reads a set of words from a file and then outputs the series of visited states for a word in the language. 
+ *
+ * Transitions are stored in a HashMap, in which the current state and transition's symbol are key and the target states are stored in Sets.
+ * 
+ * The goal of this repo is to use datastructures in the best way possible to optimise the time complexity of the program. 
+ * 
+ * @author Devops_Mika
+ */
+
+import java.util.Set;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class NFA<S, A> {
+    Map<String, Set<S>> transitions;
+
+
+    /**
+     * Constructs the Nfa and assigns states.
+     * @param states all NFA states
+     */
+	public NFA() {
+        this.transitions = new HashMap<>();
+    }
+
+    /**
+     * Adds the transitions of the given NFA
+     * @param q current state
+     * @param a transition from q to p
+     * @param p next state
+     */
+    public void addTransition(S q, A a, S p) {
+        String key = q + " " + a;
+        if (!transitions.containsKey(key)) {
+            Set<S> tar = new HashSet<>();
+            tar.add(p);
+            transitions.put(key, tar);
+        }
+        else{
+            Set<S> tar = transitions.get(key);
+            tar.add(p);
+            transitions.put(key, tar);
+        }
+    }
+
+    /**
+     * Follows the sequence of transitions starting from a given state
+     * @param q starting state
+     * @param w sequence of words
+     * @return set of all reached final states
+     */
+    public Set<S> simulate(S q, char[] w){
+        Set<S> ans = new HashSet<>();
+        ans.add(q);
+        for(int index = 0; index < w.length; index++){
+            Set<S> H = new HashSet<>();
+	        for (S st : ans){
+	            String key = st + " " + w[index];
+                Set<S> tar = transitions.get(key);
+                if (tar != null) H.addAll(tar);
+            }
+	        ans = H;
+        }
+	    return ans;
+    }
+}
